88a2aca3b0ab80202c2576a098f99b4fb3d322f1
gonsssalo <gonssalo1996@gmail.com>
2017-01-16 10:56:35 +0000

    Initial commit

---
+++ app/src/main/java/pt/ipbeja/pdm1/bejaroteirospdm2/MainActivity.java
@@ -0,0 +1,13 @@
+package pt.ipbeja.pdm1.bejaroteirospdm2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
