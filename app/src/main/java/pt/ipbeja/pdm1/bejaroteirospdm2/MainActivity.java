580c7c2ae3c548e8a7d6990803cb834f9e75cd55
gonsssalo <gonssalo1996@gmail.com>
2017-01-16 11:39:17 +0000

    fourth commit

--- app/src/main/java/pt/ipbeja/pdm1/bejaroteirospdm2/MainActivity.java
+++ app/src/main/java/pt/ipbeja/pdm1/bejaroteirospdm2/MainActivity.java
@@ -1,14 +1,37 @@
 package pt.ipbeja.pdm1.bejaroteirospdm2;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.Fragment_conteiner) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            FragmentHeadlines firstFragment = new FragmentHeadlines();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.Fragment_conteiner, firstFragment).commit();
+        }
     }
 }
