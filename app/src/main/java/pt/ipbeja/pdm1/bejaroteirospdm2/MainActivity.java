b4614661614e001e882e78345beb479976e148c4
gonsssalo <gonssalo1996@gmail.com>
2017-01-16 11:56:11 +0000

    fifth commit

--- app/src/main/java/pt/ipbeja/pdm1/bejaroteirospdm2/MainActivity.java
+++ app/src/main/java/pt/ipbeja/pdm1/bejaroteirospdm2/MainActivity.java
@@ -3,6 +3,7 @@ package pt.ipbeja.pdm1.bejaroteirospdm2;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         // Check that the activity is using the layout version with
         // the fragment_container FrameLayout
